8e7c9f18a03690a3271c03b1f914693c3fa40fb7
Diego Zamora <jaguares_diego@hotmail.com>
2025-09-05 00:44:21 -0600

    Project created

---
+++ src/main/java/io/github/dazj14/alumnia_api/AlumniaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.dazj14.alumnia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dazj14/alumnia_api/AlumniaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dazj14.alumnia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
